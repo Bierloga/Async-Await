d1abcee2a2ba21875ce3c8bb8f8bffdbd387eef1
Lucas Derksen <luc.derksen@gmail.com>
2021-11-29 19:47:55 +0100

    async await

---
+++ api-client.js
@@ -0,0 +1,12 @@
+console.log("api-client geladen")
+
+const API_KEY = "d46237a817853d71afbc937df6eb9ee3"
+const apiURL = ("https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY)
+
+async function getData() {
+    const res = await fetch(apiURL)
+    const genres = await res.json()
+    console.log(genres)
+}
+
+getData()
---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("script geladen")
+
+async function startLoading() {
+    const test = await getData()
+    console.log(test)
+}
+
