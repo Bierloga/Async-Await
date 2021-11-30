36c40267fd0937e6136c965f2e1729377684fdc4
Lucas Derksen <luc.derksen@gmail.com>
2021-11-30 16:32:38 +0100

    Async await 2

--- api-client.js
+++ api-client.js
@@ -1,12 +1,32 @@
 console.log("api-client geladen")
 
 const API_KEY = "d46237a817853d71afbc937df6eb9ee3"
-const apiURL = ("https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY)
 
-async function getData() {
-    const res = await fetch(apiURL, { method: "GET" })
+async function getMovieGenres() {
+    let genreURL = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY
+    const res = await fetch(genreURL, { method: "GET" })
     const data = await res.json()
     return data
 }
 
+async function getTopMovie() {
+    let topMovieURL = "https://api.themoviedb.org/3/find/tt0240515?api_key=d46237a817853d71afbc937df6eb9ee3&external_source=imdb_id"
+    const res = await fetch(topMovieURL, { method: "GET" })
+    const data = await res.json()
+    return data
+}
+
+async function getTopRatedActionMovies() {
+    let topRatedActionURL = "https://api.themoviedb.org/3/discover/movie?with_genres=18&sort_by=vote_average.desc&api_key=d46237a817853d71afbc937df6eb9ee3"
+    const res = await fetch(topRatedActionURL, { method: "GET" })
+    const data = await res.json()
+    return data
+}
+
+async function getMovies1975() {
+    let URL1975 = "https://api.themoviedb.org/3/discover/movie?api_key=d46237a817853d71afbc937df6eb9ee3&primary_release_year=1975"
+    const res = await fetch(URL1975, {method: "GET"})
+    const data = await res.json()
+    return data
+}
 
--- script.js
+++ script.js
@@ -1,8 +1,61 @@
 console.log("script geladen")
 
-const startLoading = async function () {
-    const data = await getData()
-    console.log(data)
-}
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const data = await getMovieGenres()
+        data.genres.forEach(function (item) {
+            const namePlusID = ("genre naam: " + item.name + " id: " + item.id);
+            const genresList = document.querySelector("#genres-list")
+            const genresListItem = document.createElement("li")
+            genresListItem.innerHTML = namePlusID
+            genresList.appendChild(genresListItem)
+        })
+    } catch (e) {
+        console.log("error")
+        console.log(e)
+    }
+})
 
-startLoading()
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const data = await getTopMovie()
+        const movieTitle = data.movie_results[0].title
+        const TopMovieTitle = document.querySelector("#top-movie")
+        TopMovieTitle.innerHTML = movieTitle
+    } catch (e) {
+        console.log("error")
+        console.log(e)
+    }
+})
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const data = await getTopRatedActionMovies()
+        data.results.forEach(function (item) {
+            const TRAtitle = item.title
+            const TRAList = document.querySelector("#top-rated-action")
+            const TRAListItem = document.createElement("li")
+            TRAListItem.innerHTML = TRAtitle
+            TRAList.appendChild(TRAListItem)
+        })
+    } catch (e) {
+        console.log("error")
+        console.log(e)
+    }
+})
+
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const data = await getMovies1975()
+        data.results.forEach(function (item) {
+            const SFtitle = item.title
+            const SFList = document.querySelector("#SF-movies")
+            const SFListItem = document.createElement("li")
+            SFListItem.innerHTML = SFtitle
+            SFList.appendChild(SFListItem)
+        })
+    } catch (e) {
+        console.log("error")
+        console.log(e)
+    }
+})
